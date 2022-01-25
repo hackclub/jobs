d67f0a5391224c7d2829de88b4235ebe0b914818
Felix Mattick <felix.mattick@gmail.com>
2022-01-25 13:47:00 -0600

    Hired events designer!

--- main.go
+++ main.go
@@ -268,10 +268,10 @@ func main() {
     	{"tech_lead.md", "https://github.com/hackclub/jobs/blob/main/directory/tech_lead.md"},
 		{"education_engineer.md", "https://github.com/hackclub/jobs/blob/main/directory/education_engineer.md"},
 		{"communications_manager.md", "https://github.com/hackclub/jobs/blob/main/directory/communications_manager.md"},
-    	{"events_designer.md", "https://github.com/hackclub/jobs/blob/main/directory/events_designer.md"},
 		{"philanthropy_position.md", "https://github.com/hackclub/jobs/blob/main/directory/philanthropy_position.md"},
 		{"executive_assistant.md", "https://github.com/hackclub/jobs/blob/main/directory/executive_assistant.md"},
-
+		
+    	{"hired_events_designer.md", "https://github.com/hackclub/jobs/blob/main/directory/events_designer.md"},
 		{"hired_clubs_lead.md", "https://gist.github.com/zachlatta/ef83904bfcfddc04bc823355e5bcd280"},
 		{"hired_bank_ops_associate.md", "https://github.com/hackclub/v3/blob/main/components/jobs/bank-ops-associate/jd.mdx"},
 		{"hired_bank_ops_lead.md", "https://github.com/hackclub/v3/blob/main/components/jobs/bank-ops-lead/jd.mdx"},
